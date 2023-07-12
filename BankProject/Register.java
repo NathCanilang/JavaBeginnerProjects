abc4a52c2525172a0bf0b28132ac1b85d3bdc41d
Nathaniel Canilang <nathcanilang@gmail.com>
2023-07-12 18:37:37 +0800

    Small Changes

--- BankProject/Register.java
+++ BankProject/Register.java
@@ -11,13 +11,15 @@ import java.util.Date;
 
 import java.awt.CardLayout;
 
+import javax.swing.ButtonGroup;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.awt.event.ActionEvent;
-import java.awt.Color;
 import javax.swing.JLabel;
 import java.awt.Font;
 import java.awt.Image;
@@ -40,20 +42,17 @@ public class Register extends JFrame {
 	 * 
 	 */
 	private static final long serialVersionUID = -117365548347710864L;
-	private Image img_ShowPass = new ImageIcon(Register.class.getResource("Images/ShowPassword.png")).getImage().getScaledInstance(45, 27,Image.SCALE_SMOOTH);
-	private Image img_ClosePass = new ImageIcon(Register.class.getResource("Images/ClosePassword.png")).getImage().getScaledInstance(45, 27,Image.SCALE_SMOOTH);
-	private JPanel contentPane;
-	private JTextField FIrstNamefld;
-	private JTextField LastNamefld;
-	private JTextField MiddleNamefld;
-	private JTextField ContacyNumfld;
-	private JTextField Emailfld;
-	private JTextField textField;
-	private JTextField Usernamefld;
-	private JPasswordField Passwordfld;
-	private JPasswordField ConPassfld;
-	private JLabel Show;
-	private JLabel Close;
+	private Image img_ShowPass = new ImageIcon(Register.class.getResource("Images/ShowPassword.png")).getImage().getScaledInstance(31,21,Image.SCALE_SMOOTH);
+	private Image img_ClosePass = new ImageIcon(Register.class.getResource("Images/ClosePassword.png")).getImage().getScaledInstance(31,21,Image.SCALE_SMOOTH);
+	private JPanel RegisterPane;
+	private JTextField FIrstNamefld, LastNamefld, MiddleNamefld, ContacyNumfld, Emailfld, textField, Usernamefld ;
+	private JLabel FirstNamelbl, LastNamelbl, MiddleNamelbl, warninglbl, Birthdaylbl, ContactNumlbl, Emaillbl, Genderlbl,IDSelectlbl,
+	IDNumberlbl, Usernamelbl, Passwordlbl, ConfirmPasswordpnl, PersonalInformationTitle, AccountCredentialsTitle, Show_1;
+	private JPasswordField Passwordfld, ConPassfld;
+	private JLabel Show, Close, IDlbl;
+	private JRadioButton MaleOption, FemaleOption
+;	
+	private JLabel Close_1;
 
 	/**
 	 * Launch the application.
@@ -80,16 +79,16 @@ public class Register extends JFrame {
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 759, 417);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		RegisterPane = new JPanel();
+		RegisterPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setLocationRelativeTo(null);
 
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
+		setContentPane(RegisterPane);
+		RegisterPane.setLayout(null);
 		
 		JLayeredPane RegisterInfo = new JLayeredPane();
 		RegisterInfo.setBounds(0, 0, 745, 380);
-		contentPane.add(RegisterInfo);
+		RegisterPane.add(RegisterInfo);
 		RegisterInfo.setLayout(new CardLayout(0, 0));
 		
 		JPanel page1 = new JPanel();
@@ -119,7 +118,6 @@ public class Register extends JFrame {
 		JButton CancelBtn = new JButton("Cancel");
 		CancelBtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
 				int choice = JOptionPane.showConfirmDialog(null, "Do you want to cancel creating account?", "Cancel Operation", JOptionPane.YES_NO_OPTION);
 				if (choice == JOptionPane.YES_OPTION) {
 					IntroToLogin.main(null);
@@ -131,57 +129,24 @@ public class Register extends JFrame {
 		CancelBtn.setBounds(10, 343, 111, 27);
 		page1.add(CancelBtn);
 		
-		JLabel PersonalInformationTitle = new JLabel("Personal Information");
+		PersonalInformationTitle = new JLabel("Personal Information");
 		PersonalInformationTitle.setFont(new Font("Tahoma", Font.PLAIN, 28));
 		PersonalInformationTitle.setBounds(10, 10, 272, 38);
 		page1.add(PersonalInformationTitle);
 		
-		JPanel FirstNamepnl = new JPanel();
-		FirstNamepnl.setBackground(new Color(128, 128, 128));
-		FirstNamepnl.setBounds(10, 93, 213, 27);
-		page1.add(FirstNamepnl);
-		FirstNamepnl.setLayout(null);
-		
-		FIrstNamefld = new JTextField();
-		FIrstNamefld.setBounds(3, 5, 206, 18);
-		FirstNamepnl.add(FIrstNamefld);
-		FIrstNamefld.setColumns(10);
-		
-		JPanel LastNamepnl = new JPanel();
-		LastNamepnl.setBackground(new Color(128, 128, 128));
-		LastNamepnl.setBounds(233, 93, 213, 27);
-		page1.add(LastNamepnl);
-		LastNamepnl.setLayout(null);
-		
-		LastNamefld = new JTextField();
-		LastNamefld.setColumns(10);
-		LastNamefld.setBounds(4, 4, 206, 18);
-		LastNamepnl.add(LastNamefld);
-		
-		JPanel MIddleNamepnl = new JPanel();
-		MIddleNamepnl.setBackground(new Color(128, 128, 128));
-		MIddleNamepnl.setBounds(456, 93, 213, 27);
-		page1.add(MIddleNamepnl);
-		MIddleNamepnl.setLayout(null);
-		
-		MiddleNamefld = new JTextField();
-		MiddleNamefld.setColumns(10);
-		MiddleNamefld.setBounds(3, 4, 206, 18);
-		MIddleNamepnl.add(MiddleNamefld);
-		
-		JLabel FirstNamelbl = new JLabel("First Name");
+		FirstNamelbl = new JLabel("First Name");
 		FirstNamelbl.setBounds(10, 58, 138, 25);
 		page1.add(FirstNamelbl);
 		
-		JLabel LastNamelbl = new JLabel("Last Name");
-		LastNamelbl.setBounds(233, 58, 138, 25);
+		LastNamelbl = new JLabel("Last Name");
+		LastNamelbl.setBounds(243, 58, 138, 25);
 		page1.add(LastNamelbl);
 		
-		JLabel MiddleNamelbl = new JLabel("Middle Name");
-		MiddleNamelbl.setBounds(456, 58, 138, 25);
+		MiddleNamelbl = new JLabel("Middle Name");
+		MiddleNamelbl.setBounds(466, 58, 138, 25);
 		page1.add(MiddleNamelbl);
 		
-		JLabel warninglbl = new JLabel("");
+		warninglbl = new JLabel("");
 		warninglbl.setBounds(10, 195, 192, 25);
 		page1.add(warninglbl);
 		
@@ -208,51 +173,77 @@ public class Register extends JFrame {
 			}
 		});
 		BrithdayChooser.setOpaque(false);
-		BrithdayChooser.setBounds(10, 158, 213, 27);
+		BrithdayChooser.setBounds(10, 158, 206, 27);
 		page1.add(BrithdayChooser);
 		
 		
-		JLabel Birthdaylbl = new JLabel("Selelct the date of your birth");
+		Birthdaylbl = new JLabel("Selelct the date of your birth");
 		Birthdaylbl.setBounds(10, 130, 162, 25);
 		page1.add(Birthdaylbl);
 		
-		JPanel ContactNumpnl = new JPanel();
-		ContactNumpnl.setLayout(null);
-		ContactNumpnl.setBackground(Color.GRAY);
-		ContactNumpnl.setBounds(233, 158, 213, 27);
-		page1.add(ContactNumpnl);
-		
-		ContacyNumfld = new JTextField();
-		ContacyNumfld.setColumns(10);
-		ContacyNumfld.setBounds(4, 4, 206, 18);
-		ContactNumpnl.add(ContacyNumfld);
-		
-		JLabel ContactNumlbl = new JLabel("Enter your contact number");
-		ContactNumlbl.setBounds(233, 130, 162, 25);
+		ContactNumlbl = new JLabel("Enter your contact number");
+		ContactNumlbl.setBounds(243, 130, 162, 25);
 		page1.add(ContactNumlbl);
 		
-		JPanel Emailpnl = new JPanel();
-		Emailpnl.setLayout(null);
-		Emailpnl.setBackground(Color.GRAY);
-		Emailpnl.setBounds(456, 158, 213, 27);
-		page1.add(Emailpnl);
+		Emaillbl = new JLabel("Enter your E-mail address");
+		Emaillbl.setBounds(466, 130, 162, 25);
+		page1.add(Emaillbl);
+		
+		Genderlbl = new JLabel("Choose your gender");
+		Genderlbl.setBounds(10, 233, 138, 27);
+		page1.add(Genderlbl);
+		
+		MaleOption = new JRadioButton("Male");
+		MaleOption.setBounds(10, 266, 60, 21);
+		page1.add(MaleOption);
+		
+		FemaleOption = new JRadioButton("Female");
+		FemaleOption.setBounds(72, 266, 76, 21);
+		page1.add(FemaleOption);
+		
+		ButtonGroup RadioButton = new ButtonGroup();
+		RadioButton.add(MaleOption);
+		RadioButton.add(FemaleOption);
+		
+		FIrstNamefld = new JTextField();
+		FIrstNamefld.setBounds(10, 93, 206, 27);
+		page1.add(FIrstNamefld);
+		FIrstNamefld.setColumns(10);
+		
+		LastNamefld = new JTextField();
+		LastNamefld.setBounds(243, 93, 206, 27);
+		page1.add(LastNamefld);
+		LastNamefld.setColumns(10);
+		
+		MiddleNamefld = new JTextField();
+		MiddleNamefld.setBounds(466, 93, 206, 27);
+		page1.add(MiddleNamefld);
+		MiddleNamefld.setColumns(10);
+		
+		ContacyNumfld = new JTextField();
+		ContacyNumfld.setBounds(243, 158, 206, 27);
+		page1.add(ContacyNumfld);
+		ContacyNumfld.setColumns(10);
 		
 		Emailfld = new JTextField();
+		Emailfld.setBounds(466, 158, 206, 27);
+		page1.add(Emailfld);
 		Emailfld.setColumns(10);
-		Emailfld.setBounds(4, 4, 206, 18);
-		Emailpnl.add(Emailfld);
 		
-		JLabel Emaillbl = new JLabel("Enter your E-mail address");
-		Emaillbl.setBounds(456, 130, 162, 25);
-		page1.add(Emaillbl);
 		
 		JButton CreateAccbtn = new JButton("Create");
 		CreateAccbtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				RegisterInfo.removeAll();
-				RegisterInfo.add(page3);
-				RegisterInfo.repaint();
-				RegisterInfo.revalidate();
+				
+				int choice = JOptionPane.showConfirmDialog(null, "Are the information that you input are finalized?", "Confirmation", JOptionPane.YES_NO_OPTION);
+				if (choice == JOptionPane.YES_OPTION) {
+					RegisterInfo.removeAll();
+					RegisterInfo.add(page3);
+					RegisterInfo.repaint();
+					RegisterInfo.revalidate();
+					
+				}
+				
 			}
 		});
 		CreateAccbtn.setBounds(624, 343, 111, 27);
@@ -270,79 +261,52 @@ public class Register extends JFrame {
 		Backbtn2.setBounds(10, 343, 111, 27);
 		page2.add(Backbtn2);
 		
-		JLabel AccountCredentialsTitle = new JLabel("Account Credentials");
+		AccountCredentialsTitle = new JLabel("Account Credentials");
 		AccountCredentialsTitle.setFont(new Font("Tahoma", Font.PLAIN, 28));
 		AccountCredentialsTitle.setBounds(4, 4, 256, 38);
 		page2.add(AccountCredentialsTitle);
 		
+		IDlbl = new JLabel("No ID Type selected");
+		IDlbl.setBounds(12, 109, 111, 27);
+		page2.add(IDlbl);
+		
 		String[] IDType = {"", "National ID", "Driver's License", "Student ID", "Company ID"};
-		JComboBox <String> IDTypeComBox = new JComboBox<>(IDType);
+		JComboBox<String> IDTypeComBox = new JComboBox<>(IDType);
+
+		IDTypeComBox.addItemListener(new ItemListener() {
+		    public void itemStateChanged(ItemEvent e) {
+		        if (e.getStateChange() == ItemEvent.SELECTED) {
+		            String selectedIDType = (String) IDTypeComBox.getSelectedItem();
+		            if (selectedIDType.equals("")) {
+		                IDlbl.setText("No ID type selected");
+		            } else {
+		                IDlbl.setText("");
+		            }
+		        }
+		    }
+		});
+
 		IDTypeComBox.setBounds(10, 82, 228, 27);
 		page2.add(IDTypeComBox);
 		
-		JLabel IDSelectlbl = new JLabel("Select ID Type");
+		IDSelectlbl = new JLabel("Select ID Type");
 		IDSelectlbl.setBounds(10, 52, 179, 27);
 		page2.add(IDSelectlbl);
 		
-		JPanel FirstNamepnl_1 = new JPanel();
-		FirstNamepnl_1.setLayout(null);
-		FirstNamepnl_1.setBackground(Color.GRAY);
-		FirstNamepnl_1.setBounds(248, 82, 322, 27);
-		page2.add(FirstNamepnl_1);
-		
-		textField = new JTextField();
-		textField.setColumns(10);
-		textField.setBounds(3, 5, 315, 18);
-		FirstNamepnl_1.add(textField);
-		
-		JLabel IDNumberlbl = new JLabel("Enter your ID Number");
+		IDNumberlbl = new JLabel("Enter your ID Number");
 		IDNumberlbl.setBounds(248, 52, 179, 27);
 		page2.add(IDNumberlbl);
 		
-		JPanel Usernamepnl = new JPanel();
-		Usernamepnl.setLayout(null);
-		Usernamepnl.setBackground(Color.GRAY);
-		Usernamepnl.setBounds(10, 154, 322, 27);
-		page2.add(Usernamepnl);
-		
-		Usernamefld = new JTextField();
-		Usernamefld.setColumns(10);
-		Usernamefld.setBounds(3, 5, 315, 18);
-		Usernamepnl.add(Usernamefld);
-		
-		JPanel Passwordpnl = new JPanel();
-		Passwordpnl.setLayout(null);
-		Passwordpnl.setBackground(Color.GRAY);
-		Passwordpnl.setBounds(342, 154, 322, 27);
-		page2.add(Passwordpnl);
-		
-		
-		Passwordfld = new JPasswordField();
-		Passwordfld.setEchoChar('*');
-		Passwordfld.setBounds(2, 2, 316, 22);
-		Passwordpnl.add(Passwordfld);
-		
-		JLabel Usernamelbl = new JLabel("Enter Username");
-		Usernamelbl.setBounds(10, 127, 179, 27);
+		 Usernamelbl = new JLabel("Enter Username");
+		Usernamelbl.setBounds(12, 156, 179, 27);
 		page2.add(Usernamelbl);
 		
-		JLabel Passwordlbl = new JLabel("Enter Password");
-		Passwordlbl.setBounds(342, 125, 179, 27);
+		Passwordlbl = new JLabel("Enter Password");
+		Passwordlbl.setBounds(250, 156, 179, 27);
 		page2.add(Passwordlbl);
 		
-		JPanel Passwordpnl_1 = new JPanel();
-		Passwordpnl_1.setLayout(null);
-		Passwordpnl_1.setBackground(Color.GRAY);
-		Passwordpnl_1.setBounds(342, 213, 322, 27);
-		page2.add(Passwordpnl_1);
-		
-		ConPassfld = new JPasswordField();
-		ConPassfld.setEchoChar('*');
-		ConPassfld.setBounds(2, 3, 316, 22);
-		Passwordpnl_1.add(ConPassfld);
-		
-		JLabel ConfirmPasswordpnl = new JLabel("Confirm Password");
-		ConfirmPasswordpnl.setBounds(343, 185, 179, 27);
+		ConfirmPasswordpnl = new JLabel("Confirm Password");
+		ConfirmPasswordpnl.setBounds(250, 205, 179, 27);
 		page2.add(ConfirmPasswordpnl);
 		
 		JRadioButton TOCradiobtn = new JRadioButton("I have read the Terms and Condition");
@@ -363,7 +327,7 @@ public class Register extends JFrame {
 				}
 			}
 		});
-		Show.setBounds(665, 154, 45, 27);
+		Show.setBounds(483, 183, 31, 21);
 		Show.setIcon(new ImageIcon(img_ShowPass));
 		page2.add(Show);
 		
@@ -379,10 +343,65 @@ public class Register extends JFrame {
 				}
 			}
 		});
-		Close.setBounds(665, 154, 45, 27);
+		Close.setBounds(483, 183, 31, 21);
 		Close.setIcon(new ImageIcon(img_ClosePass));
 		Close.setVisible(false);
 		page2.add(Close);
+		
+		textField = new JTextField();
+		textField.setBounds(248, 86, 315, 23);
+		page2.add(textField);
+		textField.setColumns(10);
+		
+		Usernamefld = new JTextField();
+		Usernamefld.setBounds(12, 183, 228, 21);
+		page2.add(Usernamefld);
+		Usernamefld.setColumns(10);
+		
+		
+		Passwordfld = new JPasswordField();
+		Passwordfld.setBounds(250, 183, 228, 21);
+		page2.add(Passwordfld);
+		Passwordfld.setEchoChar('*');
+		
+		ConPassfld = new JPasswordField();
+		ConPassfld.setBounds(250, 231, 228, 22);
+		page2.add(ConPassfld);
+		ConPassfld.setEchoChar('*');
+		
+		Show_1 = new JLabel("");
+		Show_1.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				Close_1.setVisible(true);
+			
+				if (Show_1.isVisible()) {
+					ConPassfld.setEchoChar((char)0);
+					Show_1.setVisible(false);
+				}else {	
+				}
+			}
+		});
+		Show_1.setIcon(new ImageIcon(img_ShowPass));
+		Show_1.setBounds(483, 232, 31, 21);
+		page2.add(Show_1);
+		
+		Close_1 = new JLabel("");
+		Close_1.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				Show_1.setVisible(true);
+				
+				if(Close_1.isVisible()) {
+					ConPassfld.setEchoChar('*');
+					Close_1.setVisible(false);
+				}
+			}
+		});
+		Close_1.setBounds(483, 231, 31, 21);
+		Close_1.setIcon(new ImageIcon(img_ClosePass));
+		Close_1.setVisible(false);
+		page2.add(Close_1);
 		
 		JButton backbtn = new JButton("Back to Login");
 		backbtn.addActionListener(new ActionListener() {
@@ -396,4 +415,3 @@ public class Register extends JFrame {
 		
 	}
 }
-
